2bac21d0565ae78f5a026a0f788b6ae9390f3209
Jettison <jettisonjj@gmail.com>
2024-02-04 02:24:48 +0800

    Inject DbContext w/ connectionString

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("MySQLConnection");
+builder.Services.AddDbContext<TodoContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
