3669c505e2c751dafdad6940918e66cf4ab13645
Jettison <jettisonjj@gmail.com>
2024-02-07 23:47:07 +0800

    added Get Controllers w/ CORS

--- TodoList/Controllers/TodoControllers.cs
+++ TodoList/Controllers/TodoControllers.cs
@@ -1,7 +1,6 @@
-
-
-
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoList.Data;
 using TodoList.Models;
 
@@ -17,7 +16,17 @@ namespace TodoList.Controllers
         {
             _todoContext = todoContext;
         }
+        [EnableCors("Localhost")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllTodo()
+        {
+            var todos = await _todoContext.TodoItems.ToListAsync();
 
+            return Ok(todos);
+
+        }
+
+        [EnableCors("Localhost")]
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] TodoItem todoItem)
         {
@@ -25,6 +34,16 @@ namespace TodoList.Controllers
             await _todoContext.SaveChangesAsync();
 
             return Ok(todoItem);
+        }
+
+        [EnableCors("Localhost")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(string id)
+        {
+            _todoContext.TodoItems.Remove(new TodoItem { Id = id });
+            await _todoContext.SaveChangesAsync();
+
+            return Ok(id);
         }
 
     }
