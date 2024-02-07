eb7c2d723e5c89944b23753981dcb111ce766f99
Jettison <jettisonjj@gmail.com>
2024-02-07 23:49:43 +0800

    configured POST request and added FetchData fn

--- TodoList/client/src/App.jsx
+++ TodoList/client/src/App.jsx
@@ -1,21 +1,52 @@
-
-
+import axios from "axios";
 import { useState } from "react";
 
 function App() {
-
-	// 	TODO: make a function that sends input data to the server through API
 	const [inputValue, setInputValue] = useState("");
 
 	const handleChange = (e) => {
 		setInputValue(e.target.value);
 	};
 
+	// Send Task to Server to DB
+	const sendData = async () => {
+		try {
+			if (!inputValue) {
+				throw new Error("Input value is empty");
+			}
+
+			const response = await axios.post("http://localhost:5217/api/todo", {
+				Id: crypto.randomUUID(),
+				Name: inputValue,
+				IsComplete: false,
+			});
+			console.log(response.data);
+		} catch (error) {
+			console.error("Error sending data:", error);
+		}
+	};
+	// TODO: Render the Todos from DB through components and pass the name through props and state
+	const fetchData = async () => {
+		try {
+			const response = await axios.get("http://localhost:5217/api/todo");
+
+			console.log(response.data);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	fetchData();
+
+
+
+
 	return (
 		<>
 			<div className='todo_list'>
 				<input type='text' placeholder='Add a task' value={inputValue} onChange={handleChange} />
-				<button>Add</button>
+				<button onClick={sendData}>Add</button>
+				<div className='todo_tasks'></div>
 			</div>
 		</>
 	);
