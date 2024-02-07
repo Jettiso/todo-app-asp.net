74b600c7388625100f3cf8d06bddc82106084631
Jettison <jettisonjj@gmail.com>
2024-02-07 23:51:01 +0800

    Configured CORS Policy to allow request from client side

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -5,17 +5,30 @@ using TodoList.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("MySQLConnection");
-builder.Services.AddDbContext<TodoContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))); 
+builder.Services.AddDbContext<TodoContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("Localhost",
+		policy =>
+		{
+			policy.WithOrigins("http://localhost:5173/").AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+		});
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseCors();
+app.MapControllers();
 app.Run();
