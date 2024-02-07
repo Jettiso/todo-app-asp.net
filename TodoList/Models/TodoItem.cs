846c8d6e09c64f5eac0c089e5c4cd90c32d399d5
Jettison <jettisonjj@gmail.com>
2024-02-07 23:47:47 +0800

    changed the Id from int to string due to randomUUID()

--- TodoList/Models/TodoItem.cs
+++ TodoList/Models/TodoItem.cs
@@ -3,7 +3,7 @@ namespace TodoList.Models
 {
     public class TodoItem
     {
-        public long Id { get; set; }
+        public string? Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
     }
